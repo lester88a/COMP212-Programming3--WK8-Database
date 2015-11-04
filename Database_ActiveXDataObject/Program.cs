f56e6ad35a863518126275ed51bdc9e1965cf9dc
lester88a <lester88a@gmail.com>
2015-11-04 08:50:33 -0500

    ADO.Net

---
+++ Database_ActiveXDataObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_ActiveXDataObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ADO > ActiveX Data Object
+             * DAO > Data Access Object 
+             *      Ist Link - Object
+             *      
+             * [ADO.Net for TEST2]
+             *      > A set of object - oriented libraries
+             *      > For database connect activity, also used for text files, spreadshhts.
+             *      
+             */
+        }
+    }
+}
