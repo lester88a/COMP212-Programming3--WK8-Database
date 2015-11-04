89289f48aabf3dc2921a63d827475ad8197d50cf
lester88a <lester88a@gmail.com>
2015-11-04 09:01:33 -0500

    Data providers

--- Database_ActiveXDataObject/Program.cs
+++ Database_ActiveXDataObject/Program.cs
@@ -17,7 +17,17 @@ namespace Database_ActiveXDataObject
              *      
              * [ADO.Net for TEST2]
              *      > A set of object - oriented libraries
-             *      > For database connect activity, also used for text files, spreadshhts.
+             *      > For database connectivity, also used for [text files, spreadsheets] connectivity.
+             * 
+             *  A. data providers [oracle, mysql...] ~~ Libaries:
+             *      
+             *      1. ODBC [object database connectivity] Data provider:  older databases.
+             *      2. OLEDB [object Link Embedded Database] Data provider: new dbs + Access + Excel
+             *      3. Oracle Data provider: for oracle database
+             *      4. Sql Data provider: for Microsoft SQL Sever
+             *      5. BDP [Burland] Data provider: for sql server, IBM, DB2, Oracle. 
+             *      
+             *      
              *      
              */
         }
